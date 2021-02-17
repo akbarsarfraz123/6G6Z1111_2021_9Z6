7c0624124bd2753a9ce0afec7c34e7dad752cbc1
Jacques Andre <58818181+JacAndre@users.noreply.github.com>
2021-02-17 20:02:18 +0000

    suggestion box

--- application/src/components/Search.vue
+++ application/src/components/Search.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar sticky-top navbar-expand-lg">
-    <div class="container-fluid">
+    <div class="w-100">
       <!--   <li class="nav-item">
           <button
             class="btn btn-success"
@@ -14,12 +14,17 @@
             Import
           </button>
         </li> -->
-      <form class="d-flex mx-auto input-group w-50">
-        <input
+      <form class="d-flex input-group w-50 mx-auto">
+        <div class="w-100 d-flex flex-row">
+          <input
           type="search"
-          class="form-control mx-auto"
+          class="form-control p-0"
+          id="search-bar"
           placeholder="Search"
           aria-label="Search"
+          autocomplete="off"
+          v-model="card"
+          @input="filterCards"
         />
         <button
           class="btn btn-success"
@@ -28,7 +33,13 @@
         >
           <font-awesome-icon icon="search" />
         </button>
-      </form>
+        </div>
+        <div class="w-100 d-flex flew-row p-0" v-if="filteredCards">
+          <div class="w-100">
+            <input type="text" class="list-group-item p-0" id="suggest" v-for="filteredCard in filteredCards" v-bind:key="filteredCard" :value="filteredCard" />
+          </div>
+        </div>
+        </form>
     </div>
   </nav>
 </template>
@@ -37,6 +48,19 @@
 /*import Modal from "./Modal.vue";*/
 export default {
   components: {},
+  data: function() {
+    return {
+      card: '',
+      cards: [
+        'Apple',
+        'Banana',
+        'Cantelope',
+        'Date',
+        'Elderberry'
+        ],
+      filteredCards: [],
+    };
+  },
   methods: {
     openModal() {
       const modal = document.getElementById("modal");
@@ -45,12 +69,15 @@ export default {
     closeModal() {
       const modal = document.getElementById("modal");
       modal.classList.remove("show");
+    },
+    autocomplete() {
+      alert();
+    },
+    filterCards() {
+      this.filteredCards = this.cards.filter(card => {
+        return card.toLowerCase().startsWith(this.card.toLowerCase());
+      })
     }
-  },
-  data() {
-    return {
-      search: ""
-    };
     // },
     // computed: {
     //   filteredCards: function() {
@@ -65,6 +92,11 @@ export default {
 <style scoped lang="scss">
 #mainBody {
   font-family: "Roboto", sans-serif;
+}
+
+#search-bar, #suggest{
+  width: 90%;
+  padding: 6px 12px;
 }
 
 .shadow-bottom {
